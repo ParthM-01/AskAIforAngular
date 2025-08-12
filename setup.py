30a5615e5b8d70dd160c094c01bf44bcb1bc516c
PM <98571764+ParthM-01@users.noreply.github.com>
2025-08-12 13:32:39 +0100

    Fix package discovery and requirements parsing

--- setup.py
+++ setup.py
@@ -2,27 +2,26 @@ from setuptools import find_packages, setup
 from typing import List
 
 
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    """Return the list of requirements from a file."""
+    requirements: List[str] = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = [req.strip() for req in file_obj.readlines()]
+        requirements = [req for req in requirements if req and not req.startswith('#')]
 
         if '-e .' in requirements:
             requirements.remove('-e .')
-    
+
     return requirements
-    
+
 
 setup(
     name='AskAIforAngular',
     version='0.0.1',
     author='Parth',
     author_email='parthmangukiya020@gmail.com',
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=get_requirements('requirements.txt')
     # include_package_data=True,
 )
